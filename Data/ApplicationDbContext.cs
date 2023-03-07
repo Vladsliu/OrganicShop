49da44f02584b6458958feef3ddc001ed49f5ce8
Vladsliu <vlad.sliusarch@gmail.com>
2023-03-07 21:55:58 +0100

    Online store project Preparation creating models and connections
    Online store project
    Preparation
    creating models and connections

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using OrganicShop2.Models.Data;
+
+namespace CulinaryClub.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options)
+		{
+
+		}
+		public DbSet<PagesDTO> MasterClasses { get; set; }
+    }
+}
---
+++ Models/Data/PagesDTO.cs
@@ -0,0 +1,18 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OrganicShop2.Models.Data
+{
+    public class PagesDTO
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Slug { get; set; }
+        public string? Description { get; set; }
+        public int? Sorting { get; set; }
+        public bool? HasSidebar { get; set; }
+        public string? Image { get; set; }
+    }
+}
