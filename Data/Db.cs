895f9bbd7f84b1fa4f156098138223e623c7df75
Vladsliu <vlad.sliusarch@gmail.com>
2023-06-23 22:37:38 +0200

    OrderDTO + OrderDetails
    OrderDTO + OrderDetails

--- Data/Db.cs
+++ Data/Db.cs
@@ -18,6 +18,8 @@ namespace OrganicShop2.Data
         public DbSet<UserDTO> Users { get; set; }
         public DbSet<RoleDTO> Roles { get; set; }
         public DbSet<UserRoleDTO> UserRoles { get; set; }
+        public DbSet<OrderDTO> Orders { get; set; }
+        public DbSet<OrderDetailsDTO> OrderDetails { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
---
+++ Models/Data/OrderDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OrganicShop2.Models.Data
+{
+    public class OrderDTO
+    {
+        [Key]
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual UserDTO Users { get; set; }
+
+    }
+}
