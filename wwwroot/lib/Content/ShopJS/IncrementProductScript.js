8cbbb04934e893a89d1dec2fba0b28f10041236c
Vladsliu <vlad.sliusarch@gmail.com>
2023-07-08 22:58:21 +0200

    AdminArea

--- wwwroot/lib/Content/ShopJS/IncrementProductScript.js
+++ wwwroot/lib/Content/ShopJS/IncrementProductScript.js
@@ -1,84 +1,6 @@
-﻿///* Increment product */
-//$(function () {
+﻿
 
-//    $("a.incproduct").click(function (e) {
-//        e.preventDefault();
-
-//        var productId = $(this).data("id");
-//        var url = "/cart/IncrementProduct";
-
-//        $.getJSON(url, { productId: productId }, function (data) {
-//            $("td.qty" + productId).html(data.qty);
-
-//            var price = data.qty * data.price;
-//            var priceHtml = "€" + price.toFixed(2);
-
-//            $("td.total" + productId).html(priceHtml);
-
-//            var gt = parseFloat($("td.grandtotal span").text());
-//            var grandtotal = (gt + data.price).toFixed(2);
-
-//            $("td.grandtotal span").text(grandtotal);
-//        });
-//    });
-//});
-///*-----------------------------------------------------------*/
-
-///* Decriment product */
-//$(function () {
-
-//    $("a.decproduct").click(function (e) {
-//        e.preventDefault();
-
-//        var $this = $(this);
-//        var productId = $(this).data("id");
-//        var url = "/cart/DecrementProduct";
-
-//        $.getJSON(url, { productId: productId }, function (data) {
-
-//            if (data.qty == 0) {
-//                $this.parent().fadeOut("fast", function () {
-//                    location.reload();
-//                });
-//            }
-//            else {
-//                $("td.qty" + productId).html(data.qty);
-
-//                var price = data.qty * data.price;
-//                var priceHtml = "€" + price.toFixed(2);
-
-//                $("td.total" + productId).html(priceHtml);
-
-//                var gt = parseFloat($("td.grandtotal span").text());
-//                var grandtotal = (gt - data.price).toFixed(2);
-
-//                $("td.grandtotal span").text(grandtotal);
-//            }
-//        });
-//    });
-//});
-///*-----------------------------------------------------------*/
-
-///* Remove product */
-//$(function () {
-
-//    $("a.removeproduct").click(function (e) {
-//        e.preventDefault();
-
-//        var $this = $(this);
-//        var productId = $(this).data("id");
-//        var url = "/cart/RemoveProduct";
-
-//        $.get(url, { productId: productId }, function (data) {
-//            location.reload();
-//        });
-//    });
-//});
-//        /*-----------------------------------------------------------*/
-
-
-
-/* Increment product */
+// Increment product 
 $(function () {
 
     $("a.incproduct").click(function (e) {
@@ -101,7 +23,7 @@ $(function () {
                 var grandtotal = (gt + data.price).toFixed(2);
 
                 $("td.grandtotal span").text(grandtotal);
-                /*Урок 26*/
+             
             }).done(function (data) {
                 var url2 = "/cart/PaypalPartial";
 
@@ -146,7 +68,7 @@ $(function () {
 
                         $("td.grandtotal span").text(grandtotal);
                     }
-                    /*Урок 26*/
+                   
                 }).done(function (data) {
 
                     var url2 = "/cart/PaypalPartial";
@@ -207,6 +129,4 @@ $(function () {
     });
 
 });
-/*-----------------------------------------------------------*/
-IncrementProductScript.txt
-IncrementProductScript.txt.На экране.
+
